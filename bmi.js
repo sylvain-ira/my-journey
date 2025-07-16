11a02c6815bdb7d923381dde27e41355573e5916
sylvain-ira <sylvainirabaruta@gmail.com>
2025-07-16 10:13:33 +0200

    first commit

---
+++ bmi.js
@@ -0,0 +1,30 @@
+// let weight = parseInt(prompt("enter your weightin kg "));
+// let height = parseFloat(prompt("enter your height in cm"))
+// let gender = prompt("enter your gender")
+// let bmiCategory;
+
+// let bmi = weight / (height*2);
+
+
+// if (bmi <= 18.5){
+// bmiCategory = "normal"
+// }
+// else if(bmi >18.6 && bmi<23.4){
+//     bmiCategory = "overwight"
+
+// }
+// else{
+//     bmiCategory ="obesity"
+// }
+
+// let message = gender === "m" ? `hey bro! your bmi is: ${bmi} which id ${bmiCategory}` : `hey bro! your bmi is: ${bmi} which id ${bmiCategory}`
+//  console.log(message);
+
+const container = document.getElementsByClassName("container")
+
+console.log(container[0])
+
+document .getElementById("data-form").
+addEventListener("submitt"), (event) => {
+    event.preventDefault();
+}
